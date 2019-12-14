94b1d80c72ae6a34bc9b27f54f626c90189fb78c
Daniel Jaouen <dcj24@cornell.edu>
2019-12-14 13:17:14 -0500

    Add part-2

--- sprint-challenge/aq_dashboard.py
+++ sprint-challenge/aq_dashboard.py
@@ -1,9 +1,17 @@
+import openaq
 from flask import Flask
 
 APP = Flask(__name__)
 
 
+def get_tuples():
+    api = openaq.OpenAQ()
+    measurements = api.measurements(city="Los Angeles", parameter='pm25')
+    return [(tuple_['date']['utc'], tuple_['value']) for tuple_ in measurements[1]['results']]
+
+
 @APP.route('/')
 def root():
     """Base view."""
-    return 'TODO - part 2 and beyond!'
+    tuples = get_tuples()
+    return f"{tuples}"
